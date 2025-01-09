fd564ee43105c0c2f6a5a0d19938ffddd8528136
Cole-Graham <97812256+Cole-Graham@users.noreply.github.com>
2025-01-09 08:56:16 -0800

    config changes for debugging

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from src import config, get_mod_directories
 
+# you have to run all src scripts through src package with 'python -m src.main'
+# for debugging, there is a debugger configuration in the .code-workspace file
+
 # Use the config directly as loaded by ConfigLoader
 get_mod_directories(config)  # No need to call load() again, it was done in __init__.py
